f25d770a1deb490448ed8ac746ab4c72218ddd00
Yunfei <uryu.li3@gmail.com>
2017-12-24 01:19:30 -0500

    add todo notes

--- src/actions/PawnAction.js
+++ src/actions/PawnAction.js
@@ -1,4 +1,6 @@
 export default (num)=>{
+//TODO: if num is the original 8 then minus 16, else just minus 8, if it reaches 0......then think about it
+  
   console.log(num);
   const newPos = num - 16;
   console.log(newPos)
